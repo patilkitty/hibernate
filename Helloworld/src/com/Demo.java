c95a21d011ad205e1981c327dcf99e23b1470425
patilkitty <131430274+patilkitty@users.noreply.github.com>
2024-09-27 11:51:36 +0530

    by ki

--- Helloworld/src/com/Demo.java
+++ Helloworld/src/com/Demo.java
@@ -6,8 +6,13 @@ public static void main(String[] args) {
 		System.out.println("World");
 		System.out.println("cjc");
 	System.out.println("kiran");
+	
 
 }
+	public void poo()
+	{
+		System.out.println("poo");
+	}
 	public void mam()
 	{
 		System.out.println("Code Changes Frequently");
